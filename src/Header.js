cb5f85aba96686236b4cc66c9185b50f170e222d
madi <madi@madis-MacBook-Air.local>
2020-06-19 22:21:01 -0400

    add side nav

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,13 @@
-import React, {useEffect} from 'react';
-import {Link, useLocation} from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { FaBars } from 'react-icons/fa';
+import { Link, useLocation } from 'react-router-dom';
 
 import './css/App.css';
 import './css/Header.css';
 
 import Logo from './images/logo512.png';
+
+const RESUME_GOOGLE = 'https://drive.google.com/file/d/16AKbRfD2Ou-A5SsNADsckBEPQ3k7UWdi/view?usp=sharing';
 
 const buttonName = {
   about: 'about',
@@ -18,22 +21,67 @@ const buttonName = {
 const Header = () => {
   const location = useLocation();
 
+  const buttonClicked = id => {
+    Object.values(buttonName).forEach((id) => {
+      document.getElementById(id) &&
+        document.getElementById(id).classList.remove('button-clicked');
+    });
+
+    document.getElementById(id) &&
+      document.getElementById(id).classList.add('button-clicked');
+  };
+
+  const checkEventClick = e => {
+    if (
+      document.getElementById('side-nav')
+      && document.getElementById('nav-button')
+      && !document.getElementById('side-nav').contains(e.target)
+      && !document.getElementById('nav-button').contains(e.target)
+    ) {
+      document.getElementById('nav-button') &&
+        document.getElementById('nav-button').classList.remove('nav-button-open');
+
+      document.getElementById('side-nav') &&
+        document.getElementById('side-nav').classList.remove('is-nav-open');
+    }
+  };
+
+  const handleClick = () => {
+    document.getElementById('nav-button') &&
+      document.getElementById('nav-button').classList.toggle('nav-button-open');
+
+    document.getElementById('side-nav') &&
+      document.getElementById('side-nav').classList.toggle('is-nav-open');
+  };
+
+  const sideNavButtonClicked = id => {
+    Object.values(buttonName).forEach((id) => {
+      document.getElementById(`sidenav-${id}`) &&
+        document.getElementById(`sidenav-${id}`).classList.remove('sidenav-button-clicked');
+    });
+
+    document.getElementById(`sidenav-${id}`) &&
+      document.getElementById(`sidenav-${id}`).classList.add('sidenav-button-clicked');
+  }
+
+  useEffect(() => {
+    window.addEventListener('click', checkEventClick);
+
+    return () => {
+      window.removeEventListener('click', checkEventClick);
+    };
+  }, []);
+
   useEffect(() => {
     const currentPath = location.pathname.split('/')[1];
     if (currentPath) {
       buttonClicked(currentPath);
+      sideNavButtonClicked(currentPath);
     } else {
       buttonClicked(buttonName.about);
+      sideNavButtonClicked(buttonName.about);
     }
   });
-
-  const buttonClicked = (id) => {
-    Object.values(buttonName).forEach((id) => {
-      document.getElementById(id).classList.remove('button-clicked');
-    });
-
-    document.getElementById(id).classList.add('button-clicked');
-  };
 
   return (
     <div>
@@ -98,13 +146,87 @@ const Header = () => {
             onClick={() => buttonClicked(buttonName.resume)}
           >
             <a
-              href='https://drive.google.com/file/d/16AKbRfD2Ou-A5SsNADsckBEPQ3k7UWdi/view?usp=sharing'
+              href={RESUME_GOOGLE}
               rel='noopener noreferrer'
               target='_blank'
             >
               Resume
             </a>
           </button>
+        </div>
+        <div className='sidenav-wrapper'>
+          <button
+            className='nav-button'
+            id='nav-button'
+            onClick={() => handleClick()}
+          >
+            <FaBars className='mail-icon nav-icon' />
+          </button>
+        </div>
+        <div
+          className='side-nav'
+          id='side-nav'
+        >
+          <div className='sidenav-button-wrapper'>
+            <button
+              className='sidenav-button'
+              id={`sidenav-${buttonName.about}`}
+              onClick={() => sideNavButtonClicked(buttonName.about)}
+            >
+              <Link to='/'>
+                About
+            </Link>
+            </button>
+            <button
+              className='sidenav-button'
+              id={`sidenav-${buttonName.work}`}
+              onClick={() => sideNavButtonClicked(buttonName.work)}
+            >
+              <Link to='/work'>
+                Work
+            </Link>
+            </button>
+            <button
+              className='sidenav-button'
+              id={`sidenav-${buttonName.almaMater}`}
+              onClick={() => sideNavButtonClicked(buttonName.almaMater)}
+            >
+              <Link to='/alma-mater'>
+                Alma Mater
+            </Link>
+            </button>
+            <button
+              className='sidenav-button'
+              id={`sidenav-${buttonName.projects}`}
+              onClick={() => sideNavButtonClicked(buttonName.projects)}
+            >
+              <Link to='/projects'>
+                Projects
+            </Link>
+            </button>
+            <button
+              className='sidenav-button'
+              id={`sidenav-${buttonName.honors}`}
+              onClick={() => sideNavButtonClicked(buttonName.honors)}
+            >
+              <Link to='/honors'>
+                Honors
+            </Link>
+            </button>
+            <button
+              className='sidenav-button'
+              id={`sidenav-${buttonName.resume}`}
+              onClick={() => sideNavButtonClicked(buttonName.resume)}
+            >
+              <a
+                href={RESUME_GOOGLE}
+                rel='noopener noreferrer'
+                target='_blank'
+              >
+                Resume
+            </a>
+            </button>
+          </div>
         </div>
       </header>
     </div>
